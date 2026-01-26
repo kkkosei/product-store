cf458caeab1af89066603ca21d46fa0198d789cf
kkkosei <kou505011@gmail.com>
2026-01-27 02:36:57 +0900

    feat: product_storeを依存関係に追加し、APIヘルスチェックエンドポイントを実装

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,7 @@
 import Express from "express";
 import cors from "cors";
+import path from "path";
+
 import { ENV } from "./config/env";
 import { clerkMiddleware } from '@clerk/express'
 
@@ -18,7 +20,7 @@ app.use(clerkMiddleware()); // auth obj will be attached to req
 app.use(Express.json()); // parses json requests bodies
 app.use(Express.urlencoded({ extended: true })); // parses from data (like HTML forms)
 
-app.get("/", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.json({ 
     message: "Welcome to Productify API - Powered by PostgresSQL, Dizzle ORM & Clerk Auth" ,
     endpoints: {
@@ -31,6 +33,18 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
+
+if(ENV.NODE_ENV == "production"){
+  const __dirname = path.resolve();
+
+  //serve static files from client/dist
+  app.use(Express.static(path.join(__dirname, "../client/dist")));
+
+  // handle SPA routing - send all non-API routes to index.html - react app
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
