5287d491b67e576f09ecda6724c7aa6df9a5b648
kkkosei <kou505011@gmail.com>
2026-01-26 17:15:23 +0900

    feat: コメントセクションを追加し、商品ページに統合

--- client/src/hooks/useProducts.js
+++ client/src/hooks/useProducts.js
@@ -1,5 +1,5 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
-import { createProduct, getAllProducts } from "../lib/api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createProduct, deleteProduct, getAllProducts, getProductById, getMyProducts } from "../lib/api";
 
 export const useProducts = () => {
   const result = useQuery({ queryKey: ["products"], queryFn: getAllProducts });
@@ -9,4 +9,27 @@ export const useProducts = () => {
 export const useCreateProduct = () => {
   const result = useMutation({ mutationFn: createProduct });
   return result;
+};
+
+export const useProduct = (id) => {
+  return useQuery({
+    queryKey:["product", id],
+    queryFn: () => getProductById(id),
+    enabled: !! id
+  }) 
+};
+
+export const useDeleteProduct = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: deleteProduct(),
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey:["myProducts"]})
+    }
+  });
+};
+
+export const useMyProducts = () => {
+  return useQuery({ queryKey:["myProducts"], queryFn:getMyProducts })
+
 };
