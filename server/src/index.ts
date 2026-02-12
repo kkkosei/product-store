1abcb2889910bbcf30a519126a2af1ef4a859146
kkkosei <kou505011@gmail.com>
2026-02-13 02:50:09 +0900

    refactor: product関連の用語をprojectに変更

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import { ENV } from "./config/env";
 import { clerkMiddleware } from '@clerk/express'
 
 import userRoutes from "./routes/userRoutes";
-import productRoutes from "./routes/productRoutes";
+import projectRoutes from "./routes/projectRoutes";
 import commentRoutes from "./routes/commentRoutes";
 
 const app = Express();
@@ -22,16 +22,16 @@ app.use(Express.urlencoded({ extended: true })); // parses from data (like HTML 
 
 app.get("/api/health", (req, res) => {
   res.json({ 
-    message: "Welcome to Productify API - Powered by PostgresSQL, Dizzle ORM & Clerk Auth" ,
+    message: "Welcome to Projectify API - Powered by PostgresSQL, Dizzle ORM & Clerk Auth" ,
     endpoints: {
       users: "/api/users",
-      products: "/api/products",
+      projects: "/api/projects",
       comments: "/api/comments"},
   });
 });
 
 app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
+app.use("/api/projects", projectRoutes);
 app.use("/api/comments", commentRoutes);
 
 if(ENV.NODE_ENV == "production"){
